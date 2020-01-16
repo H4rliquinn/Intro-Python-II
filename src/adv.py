ab65c01463fe76f0d25358432eef37dc1fc607d7
Jason Sonnichsen <Harliquinn@gmail.com>
2020-01-15 19:12:35 -0500

    Added Get and drop items. Text width to 60. Added more description. Added more items.

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,27 @@
+import os
+import textwrap
+
 from room import Room
 from player import Player
 # Declare all the rooms
 
 room = {
     'outside':  Room('outside', "Outside Cave Entrance",
-                     "North of you, the cave mouth beckons", ['dagger']),
+                     "North of you, the cave mouth beckons. Wind howls out from beneath the earth as you approach. You can just hear the scurrying of rats in the darkness just inside.", ['dagger']),
 
     'foyer':    Room('foyer', "Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east. A large rock dominates the northwest corner"""),
 
     'overlook': Room('overlook', "Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ['Potion of Strength']),
 
     'narrow':   Room('narrow', "Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room('treasure', "Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. There appear to be some very small rocks floating in water here. The only exit is to the south.""", ['Very Small Rocks']),
 }
 
 
@@ -53,12 +56,17 @@ player_one = Player('outside')
 
 
 def print_room():
-    print('='*40+'\n'+room[player_one.curr_room].name+'\n'+'='*40)
+    os.system('cls')
+    print('='*60+'\n'+room[player_one.curr_room].name+'\n'+'='*60)
     # * Prints the current description (the textwrap module might be useful here).
-    print(room[player_one.curr_room].description+'\n')
+    tw = textwrap.wrap(room[player_one.curr_room].description, width=60)
+    for line in tw:
+        print(line)
     if len(room[player_one.curr_room].items):
+        print('\n')
         for item in room[player_one.curr_room].items:
             print(f'There is a {item} here')
+    print('\n')
     print('Exits are: '+room[player_one.curr_room].exits+'\n')
 
 # Print Map
@@ -75,6 +83,35 @@ def print_map():
     print(top+mid_comp+bot)
 
 
+def get_item(uimp):
+    if len(uimp) == 1:
+        print("Not sure what to get!")
+    else:
+        req_item = ' '.join(uimp[1:])
+        if req_item in room[player_one.curr_room].items:
+            player_one.inventory.append(req_item)
+            room[player_one.curr_room].items.remove(req_item)
+            print_room()
+        else:
+            print("That doesn't exit here!")
+
+
+def drop_item(uimp):
+    if len(uimp) == 1:
+        print("Not sure what to drop!")
+    else:
+        req_item = ' '.join(uimp[1:])
+        if req_item in player_one.inventory:
+            player_one.inventory.remove(req_item)
+            room[player_one.curr_room].items.append(req_item)
+            print_room()
+        else:
+            print("You don't have that!")
+
+
+commands = ['q: Quit', 'n,s,e,w: Move North/South/East/West',
+            'i: Inventory', 'Get: Pickup Item', 'Drop: Drop Item', 'Push: Push Object', '? or h: This list']
+
 print_room()
 # print_map()
 while True:
@@ -89,35 +126,29 @@ while True:
     # Print an error message if the movement isn't allowed.
     #
     # If the user enters "q", quit the game.
-    commands = ['q: Quit', 'n,s,e,w: Move North/South/East/West',
-                'i: Inventory', 'Get: Pickup Item', 'Drop: Drop Item', '? or h: This list']
 
     if uimp[0] == 'q':
         break
     elif uimp[0] == 'n':
         if hasattr(room[player_one.curr_room], 'n_to'):
-            print("Moving North\n")
             player_one.curr_room = room[player_one.curr_room].n_to
             print_room()
         else:
             print("Can't move that way\n")
     elif uimp[0] == 's':
         if hasattr(room[player_one.curr_room], 's_to'):
-            print("Moving South\n")
             player_one.curr_room = room[player_one.curr_room].s_to
             print_room()
         else:
             print("Can't move that way\n")
     elif uimp[0] == 'w':
         if hasattr(room[player_one.curr_room], 'w_to'):
-            print("Moving West\n")
             player_one.curr_room = room[player_one.curr_room].w_to
             print_room()
         else:
             print("Can't move that way\n")
     elif uimp[0] == 'e':
         if hasattr(room[player_one.curr_room], 'e_to'):
-            print("Moving East\n")
             player_one.curr_room = room[player_one.curr_room].e_to
             print_room()
         else:
@@ -129,6 +160,10 @@ while True:
                 print(item)
         else:
             print('Nothing')
+    elif uimp[0] == 'get':
+        get_item(uimp)
+    elif uimp[0] == 'drop':
+        drop_item(uimp)
     elif uimp[0] == '?' or uimp[0] == 'h':
         print('Available Commands')
         for command in commands:
